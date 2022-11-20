851e7f749dffb2ff01e0a95b3881a8b7dd3c6586
Jukka Lehtniemi <jil@omoroi.fi>
2022-11-20 23:22:52 +0200

    Move to new repo with Github actions

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'colemak'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://skozl.com/s/colemak-caps.zip'
+
+$locale = 'EN'
+$arch = 'i386'
+if (Get-ProcessorBits 64) {
+    $arch = 'amd64'
+}
+$fileLocation = Join-Path $toolsDir "colemak ($locale)/Colemak_$arch.msi"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  file          = $fileLocation
+  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`" ALLUSERS=1"
+  validExitCodes= @(0, 3010, 1641)
+  softwareName  = 'colemak*'
+  checksum      = 'babdb6461663932a11480da384e5ef41024470d1dc0cc8792c0b2955398d29ac'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+Get-ChildItem -recurse $toolsDir -filter setup.exe | foreach {
+    $ignoreFile = $_.FullName + '.ignore'
+    Set-Content -Path $ignoreFile -Value ($null)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
